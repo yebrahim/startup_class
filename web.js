e56321b7a9e3b9ca55bb4ea233b0dd05bc9c23eb
Yasser Elsayed <yasser.ebrahim@gmail.com>
2013-08-08 01:50:39 +0200

    initial files. index.html

--- web.js
+++ web.js
@@ -1,12 +1,14 @@
 var express = require('express');
 var app = express();
+var fs = require('fs');
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+    var html = fs.readFileSync('index.html').toString();
+    response.send(html);
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
-  console.log("Listening on " + port);
+    console.log("Listening on " + port);
 });
