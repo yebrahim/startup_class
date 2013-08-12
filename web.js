0838a0830ad444b3a7adc3187068becb18cd8b5a
Yasser Elsayed <yasser.ebrahim@gmail.com>
2013-08-12 02:22:27 +0200

    trying express static directory

--- web.js
+++ web.js
@@ -3,6 +3,13 @@ var app = express();
 var fs = require('fs');
 app.use(express.logger());
 
+app.configure(function () {
+    app.set('port', process.env.PORT || 3000);
+    app.use(express.logger('dev'));  /* 'default', 'short', 'tiny', 'dev' */
+    app.use(express.bodyParser()),
+    app.use(express.static(path.join(__dirname, 'public')));
+});
+
 app.get('/', function(request, response) {
     var html = fs.readFileSync('index.html').toString();
     response.send(html);
